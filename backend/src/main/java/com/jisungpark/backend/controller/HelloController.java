4ec38999403708c1d68b180dcb6975ca3dfcc54f
dietken1 <dietken1@ajou.ac.kr>
2025-03-30 22:28:10 +0900

    고침

--- backend/src/main/java/com/jisungpark/backend/controller/HelloController.java
+++ backend/src/main/java/com/jisungpark/backend/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/api/hello")
     public String hello() {
-        return "Hello from JisungPark Backend!\n(~~From WJ~~)";
+        return "Hello from JisungPark Backend!\n(ㅋㅋㅋ)";
     }
 }
